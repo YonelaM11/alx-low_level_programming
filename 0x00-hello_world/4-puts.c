33923a6cfb5c1d0853500fd0373d2e47ca4ea4cc
YonelaM11 <yonelafelicia.mekile@gmail.com>
2023-10-09 01:51:35 -0700

    Hello world 0x00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,16 +10,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
-task 5
-#include <stdio.h>
-#include <stddef.h>
-
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
